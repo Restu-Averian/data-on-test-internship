aa93c26c4c2b7991d227fd552c9d74325cb8dab7
=Restu Averian Putra <=restuaverianputra123@gmail.com>
2022-07-06 12:29:42 +0700

    fix style

--- src/components/CardComp.js
+++ src/components/CardComp.js
@@ -12,14 +12,18 @@ function CardComp(props) {
     }
 
     return (
-        <div className='text-white cursor-pointer' onClick={moveToCityNews}>
-            
-            <div className='overflow-hidden '>
-                <img className=' hover:scale-110 duration-300 transition-all' src='https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80' alt=""/>
+        <div className='text-white cursor-pointer ' onClick={moveToCityNews}>
+            <div className='bg-[#33374b] p-4 my-5'>
+                <div className='overflow-hidden '>
+                    <img className=' hover:scale-110 duration-300 transition-all' src='https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80' alt=""/>
+
+                </div>
+                <p className='text-center text-2xl font-semibold'>City News</p>
+            </div>
+            <div className='x text-center'>
+                <button onClick={LogOut} className='px-4 w-full py-2 border-red-500 text-red-500 rounded-md border'>LogOut</button>
 
             </div>
-            <p className='text-center text-2xl font-semibold'>City News</p>
-            <button onClick={LogOut} className='px-4 py-2 border-red-500 text-red-500 rounded-md border'>LogOut</button>
 
         </div>
     );
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -4,12 +4,24 @@ import { useNavigate } from 'react-router-dom';
 function LoginView(props) {
     let [username,setUserName] = useState("")
     let [password,setPassword] = useState("")
+
+    let [ErrorUsername,setErrorUsername] = useState("")
+    let [ErrorPassword,setErrorPassword] = useState("")
     let navigate = useNavigate()
+   
+    function validate(field,fieldName,setErrorField){
+        if(field===""){
+            setErrorField(`${fieldName} masih kosong`)
+        }else{
+            setErrorField(``)
+           
+        }
+    }
     function handleLogin(e){
         e.preventDefault();
-        if(username===""||password===""){
-            alert("Inputan masih kosong")
-        }else{
+        validate(username,"Username",setErrorUsername)
+        validate(password,"Password",setErrorPassword)
+        if(username!==""&&password!==""){
             const data = {
                 username:username,
                 password:password
@@ -20,11 +32,17 @@ function LoginView(props) {
     }
     return (
         <div className='text-white mx-auto'>
-            <h1>Login Page</h1>
+            <h1 className='text-center font-semibold text-2xl'>Login Page</h1>
             <div className='my-5'>
                 <form onSubmit={handleLogin} className="flex flex-col space-y-5 text-[rgba(69,79,99)]">
-                    <input value={username} onChange={(e)=>setUserName(e.target.value)} type="text" placeholder='username'/>
-                    <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='password'/>
+                    <div className='flex flex-col w-full'>
+                        <input className={`input`} value={username} onChange={(e)=>setUserName(e.target.value)} type="text" placeholder='username'/>
+                        <p className={`${ErrorUsername?'':'hidden'} text-red-500`}>{ErrorUsername}</p>
+                    </div>
+                    <div className='flex flex-col w-full'>
+                        <input className={`input`} value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='password'/>
+                        <p className={`${ErrorPassword?'':'hidden'} text-red-500`}>{ErrorPassword}</p>
+                    </div>
                     <button className='bg-[#665EFF] text-white px-4 py-2'>Login</button>
                 </form>
             </div>
